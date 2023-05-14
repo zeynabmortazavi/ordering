057195f11a076380a61b0eeac186a0a0e2b82b49
zeynabmortazavi <zeynab.mortazavi95@gmail.com>
2023-05-14 21:50:48 +0330

    using mapstruct

--- ordering-api/src/main/java/com/example/converter/ControllerOrderConverter.java
+++ ordering-api/src/main/java/com/example/converter/ControllerOrderConverter.java
@@ -2,15 +2,10 @@ package com.example.converter;
 
 import com.example.domain.model.OrderDto;
 import com.example.response.OrderResponseDto;
+import org.mapstruct.Mapper;
 import org.springframework.stereotype.Component;
 
-@Component
-public class ControllerOrderConverter {
-    public OrderResponseDto ConvertToOrderResponseDto(OrderDto updateOrder) {
-        OrderResponseDto orderResponseDto = new OrderResponseDto();
-        orderResponseDto.setId(updateOrder.getId());
-        orderResponseDto.setName(updateOrder.getName());
-        orderResponseDto.setPrice(updateOrder.getPrice());
-        return orderResponseDto;
-    }
+@Mapper(componentModel = "spring")
+public interface ControllerOrderConverter {
+     OrderResponseDto ConvertToOrderResponseDto(OrderDto updateOrder);
 }
--- ordering-infrastructure/src/main/java/com/example/converter/OrderConverter.java
+++ ordering-infrastructure/src/main/java/com/example/converter/OrderConverter.java
@@ -2,23 +2,11 @@ package com.example.converter;
 
 import com.example.domain.model.OrderDto;
 import com.example.entity.Order;
+import org.mapstruct.Mapper;
 import org.springframework.stereotype.Component;
 
-@Component
-public class OrderConverter {
-    public Order convertToOrder(OrderDto orderDto) {
-        Order order = new Order();
-        order.setId(orderDto.getId());
-        order.setName(orderDto.getName());
-        order.setPrice(orderDto.getPrice());
-        return order;
-    }
-
-    public OrderDto convertToOrderDto(Order order) {
-        OrderDto orderDto = new OrderDto();
-        orderDto.setId(order.getId());
-        orderDto.setName(order.getName());
-        orderDto.setPrice(order.getPrice());
-        return orderDto;
-    }
+@Mapper(componentModel = "spring")
+public interface OrderConverter {
+     Order convertToOrder(OrderDto orderDto);
+     OrderDto convertToOrderDto(Order order) ;
 }
